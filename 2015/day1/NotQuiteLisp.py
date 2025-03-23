7f3a19dad3f69bade496a76f0ae1742b9ca09fde
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-03-23 13:19:43 +0000

    2015 day 1 part 2

--- 2015/day1/NotQuiteLisp.py
+++ 2015/day1/NotQuiteLisp.py
@@ -2,13 +2,18 @@ file = open('input.txt', 'r')
 content = file.read()
 
 def findFloor(input):
-    floor = 0
-    for instruction in input:
-        if instruction == '(':
-            floor += 1
-        elif instruction == ')':
-            floor -= 1
-    return floor
+  floor = 0
+  hasEnteredBasement = False
+  basementEntered = None
+  for index, instruction in enumerate(input, 1):
+    if instruction == '(':
+      floor += 1
+    elif instruction == ')':
+      floor -= 1
+    if floor == -1 and not hasEnteredBasement:
+      basementEntered = index
+      hasEnteredBasement = True
+  return floor, basementEntered
 
 file.close()
 
