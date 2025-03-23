91218f654d475ceefaa208634779e5e1b49d7be5
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-03-23 14:31:03 +0000

    2015 day 2 part two

--- 2015/day2/IWasToldThereWouldBeNoMath.py
+++ 2015/day2/IWasToldThereWouldBeNoMath.py
@@ -4,12 +4,21 @@ def calft2Wrapping(length, width, height):
     hl = height * length
     return 2 * (lw + wh + hl) + min(lw, wh, hl)
 
-def calTotalft2Wrapping(filename):
-    with open(filename, 'r') as file:
-        return sum(
-            calft2Wrapping(*map(int, line.strip().split('x')))
-            for line in file
-            if len(line.strip().split('x')) == 3
-        )
+def calRibbon(length, width, height):
+    smallPerimeter = sorted([length, width, height])[:2]
+    sumSmall = sum(smallPerimeter)
+    cubic = length * width * height
+    return sumSmall + sumSmall + cubic
 
-print(calTotalft2Wrapping('input.txt'))
+def calTotal(filename):
+    totalWrapping = []
+    totalRibbon = []
+    with open(filename, 'r') as file:
+        for line in file:
+            if len(line.strip().split('x')) == 3:
+                dimensions = [int(x) for x in line.strip().split('x')]
+                totalWrapping.append(calft2Wrapping(*dimensions))
+                totalRibbon.append(calRibbon(*dimensions))
+    return sum(totalWrapping), sum(totalRibbon)
+
+print(calTotal('input.txt'))
