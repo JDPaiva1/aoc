5a8cd28895d2dc0b276e706088a5292c15314b04
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-03-25 19:06:45 +0000

    2015 day 5 part two

--- 2015/day5/DoesntHeHaveInternElvesForThis.py
+++ 2015/day5/DoesntHeHaveInternElvesForThis.py
@@ -20,11 +20,32 @@ def isNice(string):
 # print(isNice('haegwjzuvuyypxyu'))
 # print(isNice('dvszwmarrgswjxmb'))
 
+def containsPairTwice(string):
+    for i in range(len(string)-1):
+        pair = string[i:i+2]
+        if pair in string[i+2:]:
+            return True
+    return False
+
+def containsLetterTwiceWithBetween(string):
+    for index, letter in enumerate(string):
+        if letter == string[index+2:index+3]:
+            return True
+    return False
+
+def isNice2(string):
+    return containsPairTwice(string) and containsLetterTwiceWithBetween(string)
+
+# print(isNice2('qjhvhtzxzqqjkmpb'))
+# print(isNice2('xxyxx'))
+# print(isNice2('uurcxstgmygtbstg'))
+# print(isNice2('ieodomkazucvgmuy'))
+
 def countNice(filename):
     nice = 0
     with open(filename, 'r') as file:
         for line in file:
-            if isNice(line.strip()):
+            if isNice2(line.strip()):
                 nice += 1
     return nice
 
