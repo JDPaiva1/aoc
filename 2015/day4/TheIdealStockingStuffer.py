73a16b99bad17c57e2f330e4c286b89a98670d49
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-03-25 18:16:29 +0000

    2015 day 4 part two

--- 2015/day4/TheIdealStockingStuffer.py
+++ 2015/day4/TheIdealStockingStuffer.py
@@ -5,14 +5,15 @@ secretKey = "yzbqklnj"
 def getHex(string):
     return hashlib.md5(string.encode('utf-8')).hexdigest()
 
-def findLongestNumber(string):
+def findLongestNumber(string, sixZeroes = False):
     number = 1
     while True:
         hexString = getHex(string + str(number))
-        if hexString[:5] == "00000":
+        if (hexString[:5] == "00000" and not sixZeroes) or (hexString[:6] == "000000" and sixZeroes):
             return number, hexString, string + str(number)
         number += 1
 
 print(findLongestNumber("abcdef"))
 print(findLongestNumber("pqrstuv"))
 print(findLongestNumber(secretKey))
+print(findLongestNumber(secretKey, True))
