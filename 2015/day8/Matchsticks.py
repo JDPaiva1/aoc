a3a138fd33f871ccf188b24b97c0752d85df7647
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-03-29 19:56:41 +0000

    2015 day 8 part two

--- 2015/day8/Matchsticks.py
+++ 2015/day8/Matchsticks.py
@@ -2,27 +2,33 @@ import re
 
 def decodeASCII(string):
     string = string[1:-1]
+    encodedCharsLength = 0
     if '\\x' in string:
         chars = re.findall(r'\\x[0-9a-fA-F]{2}', string)
+        encodedCharsLength = len(chars)
         for char in chars:
             decodeChar = chr(int(char[2:], 16))
             string = string.replace(char, decodeChar)
-    return string
+    return string, encodedCharsLength
 
 def countCharInMemory(string):
-    string = decodeASCII(string)
-    count = len(re.findall(r'\\\\|\\\"', string))
+    string, encodedCharsLength = decodeASCII(string)
+    scapedCharsLenght = string.count('\\\\') + string.count('\\"')
     string = string.replace('\\\\', '').replace('\\"', '')
-    return count + len(string)
+    return scapedCharsLenght + len(string), encodedCharsLength, scapedCharsLenght
 
 
 numberCharsOfCode = 0
-numberCharInMemory = 0
+numberCharsOfCodeEncoded = 0
+numberCharsInMemory = 0
 
 with open('input.txt') as f:
     for line in f:
         line = line.strip().replace(' ', '')
         numberCharsOfCode += len(line)
-        numberCharInMemory += countCharInMemory(line)
+        charsInMemory, encodedCharsLength, scapedCharsLenght = countCharInMemory(line)
+        numberCharsInMemory += charsInMemory
+        numberCharsOfCodeEncoded += len(line) + 4 + encodedCharsLength + (scapedCharsLenght * 2)
 
-print(numberCharsOfCode, numberCharInMemory, numberCharsOfCode - numberCharInMemory)
+print(numberCharsOfCode, numberCharsInMemory, numberCharsOfCodeEncoded)
+print(numberCharsOfCode - numberCharsInMemory, numberCharsOfCodeEncoded - numberCharsOfCode)
