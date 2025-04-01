cb68239bfbca2b937c14ceaf9e6917aa2148d7fb
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-04-01 18:43:28 +0100

    2015 day 12 part two

--- 2015/day12/JSAbacusFramework.io.py
+++ 2015/day12/JSAbacusFramework.io.py
@@ -1,4 +1,14 @@
-import re
+import re, json
+
+def removeRedObjects(data):
+    if isinstance(data, dict):
+        if "red" in data.values():
+            return None
+        return {k: removeRedObjects(v) for k, v in data.items()}
+    elif isinstance(data, list):
+        return [removeRedObjects(x) for x in data]
+    else:
+        return data
 
 def findNumbers(input):
     numbers = re.findall(r'-?\d+', input)
@@ -7,3 +17,6 @@ def findNumbers(input):
 with open('input.json') as f:
     input = f.read()
     print("Part One: ", sum(findNumbers(input)))
+    jsonData = json.loads(input)
+    cleanedData = removeRedObjects(jsonData)
+    print("Part Two: ", sum(findNumbers(json.dumps(cleanedData))))
