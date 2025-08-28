bb1b142bf8a27e8f301a3599dd54711835ef8904
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-08-28 19:27:18 +0100

    2024 day 9 part two

--- 2024/day09/DiskFragmenter.js
+++ 2024/day09/DiskFragmenter.js
@@ -23,7 +23,8 @@ function getIndividualBlocks(diskMap) {
   return blocks;
 }
 
-function moveFileBlocks(blocks) {
+function moveFileBlocks(diskMapBlocks) {
+  const blocks = [...diskMapBlocks];
   for (let i = 0; i < blocks.length; i++) {
     if (!blocks.slice(i).join("").match(/\d/)) {
       break;
@@ -53,8 +54,54 @@ function calculateChecksum(blocks) {
   return checksum;
 }
 
+// PART TWO
+
+function moveFile(diskMapBlocks) {
+  const findFreeSpace = (blocks, fileLength) => {
+    if (fileLength <= 0 || fileLength > blocks.length) return null;
+    let count = 0;
+    let start = -1;
+    for (let i = 0; i < blocks.length; i++) {
+      if (blocks[i] === ".") {
+        if (count === 0) start = i;
+        count++;
+        if (count === fileLength) return { start, end: i };
+      } else {
+        count = 0;
+        start = -1;
+      }
+    }
+    return null;
+  };
+
+  const blocks = [...diskMapBlocks];
+
+  for (let i = blocks.length - 1; i >= 0; i--) {
+    if (blocks[i] !== ".") {
+      const currectFile = blocks[i];
+      const fileBlock = {
+        start: blocks.indexOf(currectFile),
+        end: blocks.lastIndexOf(currectFile),
+      };
+      const fileLength = fileBlock.end - fileBlock.start + 1;
+      const freeSpace = findFreeSpace(blocks, fileLength);
+      if (freeSpace && freeSpace.start < fileBlock.start) {
+        for (let j = 0; j < fileLength; j++) {
+          blocks[freeSpace.start + j] = currectFile;
+          blocks[fileBlock.start + j] = ".";
+        }
+        // console.log(blocks.join(""));
+      }
+    }
+  }
+  return blocks;
+}
+
 const blocks = getIndividualBlocks(diskMap);
 // console.log(blocks.join(""));
 const movedBlocks = moveFileBlocks(blocks);
 // console.log(movedBlocks.join(""));
-console.log(calculateChecksum(movedBlocks));
+const movedFiles = moveFile(blocks);
+// console.log(movedFiles.join(""));
+console.log("Part one: filesystem checksum: ", calculateChecksum(movedBlocks));
+console.log("Part two: filesystem checksum: ", calculateChecksum(movedFiles));
