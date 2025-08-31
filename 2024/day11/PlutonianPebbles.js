caff5ff22b3049c6c8acd315c49a9e6901bbf663
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-08-31 12:49:40 +0100

    2024 day 11 part two

--- 2024/day11/PlutonianPebbles.js
+++ 2024/day11/PlutonianPebbles.js
@@ -37,4 +37,38 @@ function blinking(numOfBlinks, stonesArrangement) {
   return newStonesArrangement.length;
 }
 
+function blinkingOptimized(numOfBlinks, stonesArrangement) {
+  // Convert array to a Map that tracks count of each stone value
+  let stoneCounts = new Map();
+  for (const stone of stonesArrangement) {
+    stoneCounts.set(stone, (stoneCounts.get(stone) || 0) + 1);
+  }
+
+  for (let blink = 0; blink < numOfBlinks; blink++) {
+    const newStoneCounts = new Map();
+
+    for (const [stone, count] of stoneCounts) {
+      const transformedStones = applicateRules(stone);
+
+      for (const newStone of transformedStones) {
+        newStoneCounts.set(
+          newStone,
+          (newStoneCounts.get(newStone) || 0) + count
+        );
+      }
+    }
+
+    stoneCounts = newStoneCounts;
+  }
+
+  // Sum up all the counts to get total number of stones
+  let totalStones = 0;
+  for (const count of stoneCounts.values()) {
+    totalStones += count;
+  }
+
+  return totalStones;
+}
+
 console.log("Part 1 (25 blinks):", blinking(25, stonesArrangement));
+console.log("Part 2 (75 blinks):", blinkingOptimized(75, stonesArrangement));
