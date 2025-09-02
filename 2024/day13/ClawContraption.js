0fb4716bbdc650915252ece175533de6c6b527a0
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-09-02 20:16:57 +0100

    2024 day 13 part two

--- 2024/day13/ClawContraption.js
+++ 2024/day13/ClawContraption.js
@@ -39,31 +39,39 @@ function getInput(data) {
   return input;
 }
 
+function getNumOfAandNumOfBpush(a, b, prize) {
+  const [ax, ay] = a;
+  const [bx, by] = b;
+  const [px, py] = prize;
+  let nb = 0;
+  let na = 0;
+
+  // calculate the determinant
+  const determinant = ax * by - ay * bx;
+  if (determinant === 0) return [0, 0];
+  // calculate the number of B push
+  nb = py * ax - ay * px;
+  if (nb % determinant !== 0) return [0, 0];
+  nb = nb / determinant;
+
+  // calculate the number of A push
+  na = px - bx * nb;
+  if (na % ax !== 0) return [0, 0];
+  na = na / ax;
+
+  return [na, nb];
+}
+
 function calNumOfTokens(input) {
   const costForA = 3;
   const costForB = 1;
   let numOfTokens = 0;
 
   for (const item of input) {
-    const [ax, ay] = item.a;
-    const [bx, by] = item.b;
-    const [px, py] = item.prize;
-
-    // calculate the determinant
-    const determinant = ax * by - ay * bx;
-    if (determinant === 0) continue;
-    // calculate the number of B push
-    let nb = py * ax - ay * px;
-    if (nb % determinant !== 0) continue;
-    nb = nb / determinant;
-
-    // calculate the number of A push
-    let na = px - bx * nb;
-    if (na % ax !== 0) continue;
-    na = na / ax;
+    const [na, nb] = getNumOfAandNumOfBpush(item.a, item.b, item.prize);
 
     // if the number of A or B push is greater than 100, continue
-    if (na >= 100 || nb >= 100) continue;
+    if (na <= 0 || nb <= 0 || na >= 100 || nb >= 100) continue;
 
     numOfTokens += na * costForA + nb * costForB;
   }
@@ -71,4 +79,28 @@ function calNumOfTokens(input) {
   return numOfTokens;
 }
 
-console.log(calNumOfTokens(getInput(data)));
+function calNumOfTokensFixingConversion(input) {
+  const positionCorrection = 10000000000000;
+  const costForA = 3;
+  const costForB = 1;
+  let numOfTokens = 0;
+
+  for (const item of input) {
+    let [px, py] = item.prize;
+    // convert the prize to fix the unit conversion error
+    px += positionCorrection;
+    py += positionCorrection;
+
+    const [na, nb] = getNumOfAandNumOfBpush(item.a, item.b, [px, py]);
+
+    if (na <= 0 || nb <= 0) continue;
+
+    numOfTokens += na * costForA + nb * costForB;
+  }
+
+  return numOfTokens;
+}
+
+const input = getInput(data);
+console.log(calNumOfTokens(input));
+console.log(calNumOfTokensFixingConversion(input));
