3cc0a876c052e78f38ec3c3946aee1e4bf037a3b
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-09-14 12:18:07 +0100

    2024 day 23 part one

---
+++ 2024/23/LANParty.js
@@ -0,0 +1,46 @@
+import fs from "fs";
+
+const connections = fs.readFileSync("input.txt", "utf-8").trim().split("\n");
+
+function findConnectedComputers(connections) {
+  const connectedComputers = new Map();
+  for (const connection of connections) {
+    const [computer1, computer2] = connection.split("-");
+    if (connectedComputers.has(computer1)) {
+      connectedComputers.get(computer1).push(computer2);
+    } else {
+      connectedComputers.set(computer1, [computer2]);
+    }
+    if (connectedComputers.has(computer2)) {
+      connectedComputers.get(computer2).push(computer1);
+    } else {
+      connectedComputers.set(computer2, [computer1]);
+    }
+  }
+  console.table(connectedComputers);
+
+  const innerConnections = new Set();
+  const innerConnectionsWithT = new Set();
+  for (const [computer, connected] of connectedComputers) {
+    for (let i = 0; i < connected.length; i++) {
+      const currentComputer = connected[i];
+      const currentConnections = connectedComputers.get(currentComputer);
+
+      for (let j = i + 1; j < connected.length; j++) {
+        const nextComputer = connected[j];
+        if (currentConnections.includes(nextComputer)) {
+          const connections = [computer, currentComputer, nextComputer];
+          connections.sort();
+          innerConnections.add(connections.join(","));
+          if (computer.startsWith("t")) {
+            innerConnectionsWithT.add(connections.join(","));
+          }
+        }
+      }
+    }
+  }
+  console.log(innerConnections.size);
+  return innerConnectionsWithT.size;
+}
+
+console.log(findConnectedComputers(connections));
