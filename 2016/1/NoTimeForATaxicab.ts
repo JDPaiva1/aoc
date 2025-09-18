c87e3fe98276602ea6ac1ed117546d84d8c29dfe
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-09-18 21:29:15 +0100

    2016 day 1 part two

--- 2016/1/NoTimeForATaxicab.ts
+++ 2016/1/NoTimeForATaxicab.ts
@@ -8,10 +8,12 @@ function parseSequence(sequence: string) {
     .map((direction) => [direction[0], Number(direction.slice(1))]);
 }
 
-function calculateDistance(sequence: string) {
+function calculateDistance(sequence: string, isPart2: boolean = false) {
   const parsedSequence = parseSequence(sequence);
   const position = [0, 0];
   let direction = [0, 1];
+
+  const seen = new Set();
 
   for (const [turn, steps] of parsedSequence) {
     let dx, dy;
@@ -24,11 +26,20 @@ function calculateDistance(sequence: string) {
     }
     direction = [dx, dy];
 
-    position[0] += direction[0] * Number(steps);
-    position[1] += direction[1] * Number(steps);
+    for (let i = 0; i < Number(steps); i++) {
+      const key = `${position[0]},${position[1]}`;
+      if (seen.has(key) && isPart2) {
+        return position.reduce((acc, curr) => acc + Math.abs(curr), 0);
+      }
+      seen.add(key);
+
+      position[0] += direction[0];
+      position[1] += direction[1];
+    }
   }
 
   return position.reduce((acc, curr) => acc + Math.abs(curr), 0);
 }
 
-console.log(calculateDistance(sequence));
+console.log("Part 1:", calculateDistance(sequence));
+console.log("Part 2:", calculateDistance(sequence, true));
