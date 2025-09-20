3404c0a0c101a5f8bda6d8caacaaa309e904c2b7
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-09-20 12:17:07 +0100

    2016 day 3 part two

--- 2016/3/SquaresWithThreeSides.ts
+++ 2016/3/SquaresWithThreeSides.ts
@@ -1,16 +1,43 @@
 import { readFileSync } from "fs";
 
-const triangles = readFileSync("input.txt", "utf-8").trim().split("\n");
+const triangles = readFileSync("input.txt", "utf-8")
+  .trim()
+  .split("\n")
+  .map((line) =>
+    line
+      .trim()
+      .split(" ")
+      .filter((item) => item.trim() !== "")
+      .map(Number)
+  );
 
 function isValidTriangle(a: number, b: number, c: number) {
   return a + b > c && a + c > b && b + c > a;
 }
 
-function getValidTriangles(triangles: string[]) {
+function getValidTriangles(triangles: number[][]) {
   const validTriangles = triangles.filter((triangle) => {
-    const [a, b, c] = triangle.trim().split("  ").map(Number);
+    const [a, b, c] = triangle;
     return isValidTriangle(a, b, c);
   });
   return validTriangles.length;
 }
+
+function getValidTrianglesInColumns(triangles: number[][]) {
+  const validTriangles = [];
+  for (let i = 0; i < triangles.length - 2; i += 3) {
+    const a = triangles[i];
+    const b = triangles[i + 1];
+    const c = triangles[i + 2];
+
+    for (let j = 0; j < 3; j++) {
+      if (isValidTriangle(a[j], b[j], c[j])) {
+        validTriangles.push([a[j], b[j], c[j]]);
+      }
+    }
+  }
+  return validTriangles.length;
+}
+
 console.log("Part 1:", getValidTriangles(triangles));
+console.log("Part 2:", getValidTrianglesInColumns(triangles));
