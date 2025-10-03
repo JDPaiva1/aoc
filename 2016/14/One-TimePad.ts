1da18fe6feb4c2b97ab5785c8e287b628689d44c
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-10-03 17:00:07 +0100

    2016 day 14 part two

--- 2016/14/One-TimePad.ts
+++ 2016/14/One-TimePad.ts
@@ -10,18 +10,28 @@ function getHash(salt: string, index: number) {
     .digest("hex");
 }
 
+function getHashStretched(salt: string, index: number) {
+  let hash = getHash(salt, index);
+  for (let i = 0; i < 2016; i++) {
+    hash = crypto.createHash("md5").update(hash).digest("hex");
+  }
+  return hash;
+}
+
 function firstTripleChar(s: string) {
   const m = s.match(/(.)\1\1/s);
   return m ? m[1] : null;
 }
 
-function getIndex64(salt: string) {
+function getIndex64(salt: string, keyStretching = false) {
   let index = 0;
   const possibleKeys: Map<number, string> = new Map();
   const keys: number[] = [];
 
   while (keys.length < 65) {
-    const hash = getHash(salt, index);
+    const hash = keyStretching
+      ? getHashStretched(salt, index)
+      : getHash(salt, index);
 
     const tripleChar = firstTripleChar(hash);
     if (tripleChar) {
@@ -51,3 +61,4 @@ function getIndex64(salt: string) {
 }
 
 console.log("Part 1:", getIndex64(salt));
+console.log("Part 2:", getIndex64(salt, true));
