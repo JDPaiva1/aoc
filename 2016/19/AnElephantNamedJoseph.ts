1bdc40c35ac1be50f565fe253549e208dbd5ce2f
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-10-09 19:01:34 +0100

    2016 day 19 part two

--- 2016/19/AnElephantNamedJoseph.ts
+++ 2016/19/AnElephantNamedJoseph.ts
@@ -16,4 +16,36 @@ function whoGetsAllTheGifts(numOfElves: number) {
   return current;
 }
 
+function whoGetsAllTheGiftsPart2(numOfElves: number) {
+  const next = new Uint32Array(numOfElves + 1);
+
+  for (let i = 1; i < numOfElves; i++) {
+    next[i] = i + 1;
+  }
+  next[numOfElves] = 1;
+
+  let remaining = numOfElves;
+  let current = 1;
+
+  let acrossPrev = current;
+  for (let i = 0; i < Math.floor(remaining / 2) - 1; i++) {
+    acrossPrev = next[acrossPrev];
+  }
+
+  while (remaining > 1) {
+    const removed = next[acrossPrev];
+    next[acrossPrev] = next[removed];
+    remaining--;
+
+    if (remaining % 2 === 0) {
+      acrossPrev = next[acrossPrev];
+    }
+
+    current = next[current];
+  }
+
+  return current;
+}
+
 console.log("Part 1:", whoGetsAllTheGifts(numOfElves));
+console.log("Part 2:", whoGetsAllTheGiftsPart2(numOfElves));
