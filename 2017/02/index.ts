b449f7de71a9c06f819a7d1b8c74866ae1d8f440
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-10-19 11:03:10 +0100

    2017 day 2 part two

--- 2017/02/index.ts
+++ 2017/02/index.ts
@@ -10,4 +10,24 @@ function getChecksum(input: string) {
   );
 }
 
+function getChecksumPart2(input: string) {
+  const rows = input.split("\n").map((row) => row.split(/\s+/).map(Number));
+  const getEvenlyDivisible = (row: number[]) => {
+    for (const [index, num] of row.entries()) {
+      for (const [otherIndex, otherNum] of row.entries()) {
+        if (index === otherIndex || num % otherNum !== 0) continue;
+        return num / otherNum;
+      }
+    }
+    return 0;
+  };
+
+  let sum = 0;
+  for (const row of rows) {
+    sum += getEvenlyDivisible(row);
+  }
+  return sum;
+}
+
 console.log("Part 1:", getChecksum(input));
+console.log("Part 2:", getChecksumPart2(input));
