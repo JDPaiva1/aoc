c0385ef6aa9b4056a3db63e8526ee635f055092e
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-10-21 19:23:15 +0100

    2017 day 4 part two

--- 2017/04/index.ts
+++ 2017/04/index.ts
@@ -4,15 +4,22 @@ const passphraseList = readFileSync("2017/04/input.txt", "utf-8")
   .trim()
   .split("\n");
 
-function getValidPassphrases(passphraseList: string[]) {
+function getValidPassphrases(passphraseList: string[], isAnagram = false) {
   let validPassphrases = 0;
   for (const passphrase of passphraseList) {
     const words = passphrase.split(" ");
-    const uniqueWords = new Set(words);
-    if (words.length !== uniqueWords.size) continue;
+    if (isAnagram) {
+      const sortedWords = words.map((word) => word.split("").sort().join(""));
+      const uniqueSortedWords = new Set(sortedWords);
+      if (sortedWords.length !== uniqueSortedWords.size) continue;
+    } else {
+      const uniqueWords = new Set(words);
+      if (words.length !== uniqueWords.size) continue;
+    }
     validPassphrases++;
   }
   return validPassphrases;
 }
 
 console.log("Part 1: " + getValidPassphrases(passphraseList));
+console.log("Part 2: " + getValidPassphrases(passphraseList, true));
