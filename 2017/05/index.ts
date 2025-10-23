0884bcad9bdf158d2ccceebebd1550e425c4e521
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-10-23 19:03:49 +0100

    2017 day 5 part two

--- 2017/05/index.ts
+++ 2017/05/index.ts
@@ -2,13 +2,13 @@ import { readFileSync } from "fs";
 
 const input = readFileSync("./2017/05/input.txt", "utf-8").trim().split("\n");
 
-function getSteps(input: string[]) {
+function getSteps(input: string[], isStrange = false) {
   const offsets = input.map(Number);
   let steps = 0;
   let pointer = 0;
   while (pointer >= 0 && pointer < offsets.length) {
     const offset = offsets[pointer];
-    offsets[pointer]++;
+    offsets[pointer] = isStrange && offset >= 3 ? offset - 1 : offset + 1;
     pointer += offset;
     steps++;
   }
@@ -16,3 +16,4 @@ function getSteps(input: string[]) {
 }
 
 console.log("Part 1:", getSteps(input));
+console.log("Part 2:", getSteps(input, true));
