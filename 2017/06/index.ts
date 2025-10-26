41beb263ca2e1fea47c894ab7eeb0ea180ebee3f
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-10-26 12:09:57 +0000

    2017 day 6 part two

--- 2017/06/index.ts
+++ 2017/06/index.ts
@@ -9,8 +9,23 @@ function getMemoryBanks(input: string) {
 function redistributionCycles(memoryBanks: number[]) {
   const seen = new Set<string>();
   let cycles = 0;
+  const state = {
+    firstSeen: { memoryBank: "", cycles: 0 },
+    secondSeen: { memoryBank: "", cycles: 0 },
+  };
   while (true) {
-    if (seen.has(memoryBanks.join(","))) break;
+    if (
+      state.firstSeen.memoryBank === memoryBanks.join(",") &&
+      state.firstSeen.cycles > 0
+    ) {
+      state.secondSeen.memoryBank = memoryBanks.join(",");
+      state.secondSeen.cycles = cycles;
+      break;
+    }
+    if (seen.has(memoryBanks.join(",")) && state.firstSeen.cycles === 0) {
+      state.firstSeen.memoryBank = memoryBanks.join(",");
+      state.firstSeen.cycles = cycles;
+    }
     seen.add(memoryBanks.join(","));
     const max = Math.max(...memoryBanks);
     const index = memoryBanks.indexOf(max);
@@ -22,7 +37,10 @@ function redistributionCycles(memoryBanks: number[]) {
     }
     cycles++;
   }
-  return cycles;
+  return [
+    state.firstSeen.cycles,
+    state.secondSeen.cycles - state.firstSeen.cycles,
+  ];
 }
 
 console.log(redistributionCycles(getMemoryBanks(input)));
