237f15b33e9adb6b3e111e9871c9527460d7374c
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-12-01 09:37:03 +0000

    2025 day 1 part two

--- 2025/01/index.ts
+++ 2025/01/index.ts
@@ -1,6 +1,8 @@
 import { readFileSync } from "fs";
 
-const rotations = readFileSync("./2025/01/input.txt", "utf8").trim().split("\n");
+const rotations = readFileSync("./2025/01/input.txt", "utf8")
+  .trim()
+  .split("\n");
 
 function partOne(rotations: string[]): number {
   const [min, max] = [0, 100];
@@ -22,4 +24,26 @@ function partOne(rotations: string[]): number {
   return count;
 }
 
+function partTwo(rotations: string[]): number {
+  const [min, max] = [0, 100];
+  let count = 0;
+  let position = 50;
+  for (let i = 0; i < rotations.length; i++) {
+    if (rotations[i].trim() === "") break;
+    const direction = rotations[i].slice(0, 1);
+    const num = Number(rotations[i].slice(1));
+    if (direction === "L") {
+      const distanceFromZero = (max - position) % max;
+      count += Math.floor((distanceFromZero + num) / max);
+      position = (((position - num) % max) + max) % max;
+    } else {
+      const total = position + num;
+      count += Math.floor(total / max);
+      position = total % max;
+    }
+  }
+  return count;
+}
+
 console.log("Part 1:", partOne(rotations));
+console.log("Part 2:", partTwo(rotations));
