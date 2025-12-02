9a99082742ef69a53d95200d4fd8f2aaea308d6c
Jose Daniel Paiva <josedaniel@jdpaiva.me>
2025-12-02 06:42:07 +0000

    2025 day 2 part two

--- 2025/02/index.ts
+++ 2025/02/index.ts
@@ -21,4 +21,25 @@ function partOne(idRanges: number[][]): number {
   return invalidIdsFound;
 }
 
+function partTwo(idRanges: number[][]): number {
+  let invalidIdsFound = 0;
+  for (const [min, max] of idRanges) {
+    for (let i = min; i <= max; i++) {
+      const str = i.toString();
+      for (let size = 1; size <= str.length / 2; size++) {
+        const pattern = str.slice(0, size);
+        const repetitions = str.length / size;
+        if (!Number.isInteger(repetitions)) continue;
+        const built = pattern.repeat(repetitions);
+        if (built === str) {
+          invalidIdsFound += i;
+          break;
+        }
+      }
+    }
+  }
+  return invalidIdsFound;
+}
+
 console.log("Part 1:", partOne(idRanges));
+console.log("Part 2:", partTwo(idRanges));
